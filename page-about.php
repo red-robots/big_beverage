245106c74e7da05808c7f8bfbd83d668975c6e45
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-09-23 01:46:58 -0400

    Styled about page

--- page-about.php
+++ page-about.php
@@ -39,11 +39,11 @@ get_header(); ?>
              <div class="fcol textcol">
                 <div class="inner">
                   <?php if ($row1_title) { ?>
-                    <h2 class="col-title"><?php echo $row1_title ?></h2>
+                    <h2 class="col-title blue"><?php echo $row1_title ?></h2>
                   <?php } ?>
 
                   <?php if ($row1_text) { ?>
-                    <div class="text"><?php echo $row1_text ?></div>
+                    <div class="text"><?php echo anti_email_spam($row1_text) ?></div>
                   <?php } ?>
                 </div>
              </div> 
@@ -53,8 +53,108 @@ get_header(); ?>
       </div>
       <?php } ?>
 
+
+      <?php  
+      $row2_image = get_field("row2_image");
+      $row2_title = get_field("row2_title");
+      $row2_text = get_field("row2_text");
+      $row2_class = ( $row2_image && ($row2_title || $row2_text) ) ? 'half':'full';
+      if( $row2_image || ($row2_title || $row2_text) ) { ?>
+      <div class="row2 section <?php echo $row2_class ?>">
+        <div class="row-inner">
+          <div class="wrapper">
+            <?php if ($row2_title || $row2_text) { ?>
+            <div class="flexwrap">
+              <div class="textcol">
+                <div class="wrap">
+                  <?php if ($row2_title) { ?>
+                  <h2 class="col-title blue"><?php echo $row2_title ?></h2> 
+                  <?php } ?>
+
+                  <?php if ($row2_text) { ?>
+                  <div class="text"><?php echo anti_email_spam($row2_text) ?></div> 
+                  <?php } ?>
+                </div>
+              </div>
+            </div>
+            <?php } ?>
+          </div>
+
+          <?php if ( $row2_image ) { ?>
+          <div class="imagewrap">
+            <div class="inner">
+              <div class="img" style="background-image:url('<?php echo $row2_image['url'] ?>')">
+                <img src="<?php echo $row2_image['url'] ?>" alt="<?php echo $row2_image['title'] ?>">
+              </div>
+            </div>
+          </div>
+          <?php } ?>
+        </div>
+      </div>
+      <?php } ?>
+
+
+      <?php  
+      $row3_image = get_field("row3_image");
+      $row3_text = get_field("row3_text");
+      $row3_class = ( $row2_image && $row3_text ) ? 'half':'full';
+      if( $row3_image || $row3_text ) { ?>
+      <div class="row3 section <?php echo $row3_class ?>">
+        <div class="flexwrap">
+          <?php if ($row3_text) { ?>
+          <div class="fcol textcol">
+            <?php if ($row3_text) { ?>
+              <div class="text"><?php echo anti_email_spam($row3_text) ?></div> 
+            <?php } ?>
+          </div>
+          <?php } ?>
+
+          <?php if ($row3_image) { ?>
+          <div class="fcol imagecol">
+            <div class="img" style="background-image:url('<?php echo $row3_image['url'] ?>')">
+              <img src="<?php echo $row3_image['url'] ?>" alt="<?php echo $row3_image['title'] ?>">
+            </div>
+          </div>
+          <?php } ?>
+        </div>
+      </div>
+      <?php } ?>
+
+
+      <?php  
+      $row4_text = get_field("row4_text");
+      $row4_button = get_field("row4_button");
+      $row4BtnTarget = ( isset($row4_button['target']) && $row4_button['target'] ) ? $row4_button['target'] : '_self';
+      $row4BtnTitle = ( isset($row4_button['title']) && $row4_button['title'] ) ? $row4_button['title'] : '';
+      $row4BtnLink = ( isset($row4_button['url']) && $row4_button['url'] ) ? $row4_button['url'] : '';
+      if( $row4_text ) { ?>
+      <div class="row4 section bubble-bg bottom">
+        <div class="row4-inner">
+          <div class="wrapper text-center">
+            <div class="fcol textcol">
+              <?php if ($row4_text) { ?>
+                <div class="text"><?php echo anti_email_spam($row4_text) ?></div> 
+              <?php } ?>
+
+              <?php if ($row4BtnTitle && $row4BtnLink) { ?>
+                <div class="button"><a href="<?php echo $row4BtnLink ?>" target="<?php echo $row4BtnTarget ?>" class="btn-outline"><?php echo $row4BtnTitle ?></a></div> 
+              <?php } ?>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php } ?>
+
     <?php endwhile; ?>	
 	</main>
 </div>
+<script>
+jQuery(document).ready(function($){
+  // if( $(".imagewrap").length ) {
+  //   var img_height = $(".imagewrap").height();
+  //   $(".imagewrap div.img").css('height',img_height+'px');
+  // }
+});
+</script>
 <?php
 get_footer();
